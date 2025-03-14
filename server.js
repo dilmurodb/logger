6e79601d761d5f9b507a111b4f509a4721414104
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-03-14 14:13:30 +0500

    feat: add built in middleware

--- server.js
+++ server.js
@@ -5,19 +5,55 @@ const app = express()
 
 const PORT = process.env.PORT || 3500
 
+// Built in middleware to handle urlencoded data
+// in other words, form data:
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }))
+
+
+//built in middleware for json
+app.use(express.json())
+
+//serve static files
+// app.use(express.static(path.join(__dirname, '/public')))
+
+
 app.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile('./views/index.html', { root: __dirname})
 })
 
-app.get('/404(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', '404.html'))
-})
 app.get('/new-page(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
 })
 
 app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(302, '/404.html')
+    res.redirect(301, '/new-page.html')
+})
+
+app.get('/prev-page(.html)?', (req, res) => {
+    res.redirect('/new-page.html') // default 302 status code
+})
+
+
+
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+}
+
+app.get('/chain(.html)?', [one, two, three])
+
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
