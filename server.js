3a6f679499e56b23113ea37a49fd4dc3b7b63071
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-03-19 19:45:13 +0500

    feat: implement auth and registration functionality

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.use('/subdir', express.static(path.join(__dirname, '/public')))
 
 
 app.use('/', require('./routes/root'))
+app.use('/register', require('./routes/register'))
+app.use('/auth', require('./routes/auth'))
 app.use('/subdir', require('./routes/subdir'))
 app.use('/employees', require('./routes/api/employees'))
 
